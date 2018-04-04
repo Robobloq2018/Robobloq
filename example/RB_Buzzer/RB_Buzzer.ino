74041860fe1397c63b211da877657b3e9e9fada4
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-04-04 14:48:17 +0800

    Create RB_Buzzer.ino

---
+++ example/RB_Buzzer/RB_Buzzer.ino
@@ -0,0 +1,21 @@
+#include "RB_BUZZER.h"
+
+
+RB_Buzzer Buzzer(36);
+
+
+void setup() {
+  // put your setup code here, to run once:
+      for (int thisNote = 0; thisNote < 29; thisNote++) {
+                  int noteDuration = 1000/noteDurations[thisNote];
+                  Buzzer.tone(melody[thisNote],noteDuration);
+                  int pauseBetweenNotes = noteDuration*0.6 ;
+                  delay(pauseBetweenNotes);
+                  Buzzer.noTone();
+                 } 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
