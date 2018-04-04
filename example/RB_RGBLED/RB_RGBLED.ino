b2f328ea3efb6495531a908f67fdeadba2dd1298
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-04-04 12:32:42 +0800

    Update RB_RGBLED.ino

--- example/RB_RGBLED/RB_RGBLED.ino
+++ example/RB_RGBLED/RB_RGBLED.ino
@@ -2,31 +2,31 @@
 RB_RGBLed RGBLED(37,2);
 void setup() {
   // put your setup code here, to run once:
-   RGBLED.setColor(1.50,0,0);
+   RGBLED.setColor(1,50,0,0);
    delay(1000);
-   RGBLED.setColor(1.0,50,0);
+   RGBLED.setColor(1,0,50,0);
    delay(1000);
-   RGBLED.setColor(1.0,0,50);
+   RGBLED.setColor(1,0,0,50);
    delay(1000);
-   RGBLED.setColor(1.0,0,0);
-   delay(1000);
-   
-    RGBLED.setColor(2.50,0,0);
-   delay(1000);
-   RGBLED.setColor(2.0,50,0);
-   delay(1000);
-   RGBLED.setColor(2.0,0,50);
-   delay(1000);
-   RGBLED.setColor(2.0,0,0);
+   RGBLED.setColor(1,0,0,0);
    delay(1000);
    
-    RGBLED.setColor(0.50,0,0);
+    RGBLED.setColor(2,50,0,0);
    delay(1000);
-   RGBLED.setColor(0.0,50,0);
+   RGBLED.setColor(2,0,50,0);
    delay(1000);
-   RGBLED.setColor(0.0,0,50);
+   RGBLED.setColor(2,0,0,50);
    delay(1000);
-   RGBLED.setColor(0.0,0,0);
+   RGBLED.setColor(2,0,0,0);
+   delay(1000);
+   
+    RGBLED.setColor(0,50,0,0);
+   delay(1000);
+   RGBLED.setColor(0,0,50,0);
+   delay(1000);
+   RGBLED.setColor(0,0,0,50);
+   delay(1000);
+   RGBLED.setColor(0,0,0,0);
    delay(1000);
 }
 
