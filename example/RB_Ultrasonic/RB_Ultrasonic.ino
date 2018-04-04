86a355933e63434060c5e8c17b3de996ad597c06
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-04-04 11:42:57 +0800

    Create RB_Ultrasonic.ino

---
+++ example/RB_Ultrasonic/RB_Ultrasonic.ino
@@ -0,0 +1,21 @@
+
+RB_Ultrasonic  ul(2);
+double ul_distance =0;
+
+void setup() {
+  // put your setup code here, to run once:
+    ul.RB_Ultrasonic_SetRGB(0x40,0xA1,200,0,0);
+    delay(1000);
+    ul.RB_Ultrasonic_SetRGB(0x40,0xA1,0,200,0);
+    delay(1000);
+     ul.RB_Ultrasonic_SetRGB(0x40,0xA1,0,0,200);
+    delay(1000);
+    Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+   ul_distance = ul.Uldistance();
+   Serial.println(ul_distance);
+   delay(500);
+}
