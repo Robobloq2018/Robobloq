3393e994e5adc0cec1e2e616138544bc616efc10
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-04-04 11:40:09 +0800

    Create RB_RGBLED.ino

---
+++ example/RB_RGBLED/RB_RGBLED.ino
@@ -0,0 +1,36 @@
+#include "RB_RGBLED.h"
+RB_RGBLed RGBLED(37,2);
+void setup() {
+  // put your setup code here, to run once:
+   RGBLED.setColor(1.50,0,0);
+   delay(1000);
+   RGBLED.setColor(1.0,50,0);
+   delay(1000);
+   RGBLED.setColor(1.0,0,50);
+   delay(1000);
+   RGBLED.setColor(1.0,0,0);
+   delay(1000);
+   
+    RGBLED.setColor(2.50,0,0);
+   delay(1000);
+   RGBLED.setColor(2.0,50,0);
+   delay(1000);
+   RGBLED.setColor(2.0,0,50);
+   delay(1000);
+   RGBLED.setColor(2.0,0,0);
+   delay(1000);
+   
+    RGBLED.setColor(0.50,0,0);
+   delay(1000);
+   RGBLED.setColor(0.0,50,0);
+   delay(1000);
+   RGBLED.setColor(0.0,0,50);
+   delay(1000);
+   RGBLED.setColor(0.0,0,0);
+   delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
