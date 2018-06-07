b775f5cb744cd6bce6b5d948c794845e2c682e37
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-07 14:34:43 +0800

    Create MP3.h

---
+++ src/MP3.h
@@ -0,0 +1,30 @@
+#ifndef RB_MP3_H
+#define RB_MP3_H
+
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <Arduino.h>
+
+#include "RB_Port.h" 
+#include <SoftwareSerial.h>
+
+
+class RB_MP3:public RB_Port,public SoftwareSerial
+{
+	public:
+	   RB_MP3(void);
+	   RB_MP3(uint8_t port);
+     void RB_MP3_Star(uint8_t mode ,uint8_t data); 
+     uint8_t  RB_MP3_Mode(void);
+     void     RB_MP3_Set(uint8_t mode);
+ protected:
+    uint8_t _mode;
+
+};
+
+
+
+
+
+#endif
