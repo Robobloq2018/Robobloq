d7855c9345c3f42fd72b9e02c513920bd1334998
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-07 14:50:01 +0800

    Update and rename RB_DCMotor to RB_DCMotor.ino

---
+++ example/RB_DCMotor/RB_DCMotor.ino
@@ -0,0 +1,23 @@
+#include "RB_DCMotor.h"
+
+RB_DCMotor  RB_DCMotor(1);
+
+void setup()
+{
+    Serial.begin(9600); 
+   
+    RB_DCMotor.RB_DCMotor_SetSpeed(0x50,0xA3,0X01,0X01,80);
+    delay(500);
+    RB_DCMotor.RB_DCMotor_SetSpeed(0x50,0xA3,0X02,0X00,80);
+    delay(500);
+    RB_DCMotor.RB_DCMotor_SetSpeed(0x50,0xA3,0X01,0X01,80);
+    delay(500);
+    RB_DCMotor.RB_DCMotor_SetSpeed(0x50,0xA3,0X02,0X00,80);
+    delay(500);
+}
+
+void loop()
+{
+
+
+}
