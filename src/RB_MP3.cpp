245517ff911cb8f29f3a81185894ff5d0eb3ddf7
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-07 14:34:18 +0800

    Create RB_MP3.cpp

---
+++ src/RB_MP3.cpp
@@ -0,0 +1,63 @@
+#include "RB_MP3.h"
+
+unsigned char Senddata[10];
+
+
+RB_MP3::RB_MP3(void):RB_Port(),SoftwareSerial(NC, NC)
+{
+	
+	
+	
+}
+
+
+RB_MP3::RB_MP3(uint8_t port):RB_Port(port),SoftwareSerial(RBPort[port].clk,RBPort[port].dat)
+{
+	_mode = 3;
+	SoftwareSerial::begin(38400);
+  
+	
+}
+uint8_t  RB_MP3::RB_MP3_Mode(void)
+{
+   return _mode;   
+  
+}
+void    RB_MP3::RB_MP3_Set(uint8_t mode)
+{
+  unsigned char i =0 ,check = 0;
+  _mode = mode ;
+  Senddata[0] = 0xAA;
+  Senddata[1] = 0x02;
+  Senddata[2] = 0x01;
+  Senddata[3] = mode;
+  for(i=0;i<4;i++)
+  {
+    check = (check^Senddata[i]);
+  }
+  Senddata[4] =~check;
+  for(i=0;i<5;i++)
+  {
+   SoftwareSerial::write(Senddata[i]);
+    }
+  _mode = mode;
+  
+}
+void RB_MP3::RB_MP3_Star(uint8_t mode ,uint8_t data)
+{   
+
+     unsigned char i =0 ,check = 0;
+	Senddata[0] = 0xAA;
+	Senddata[1] = mode;
+	Senddata[2] = 0x01;
+	Senddata[3] = data;
+	for(i=0;i<4;i++)
+	{
+		check = (check^Senddata[i]);
+	}
+	Senddata[4] =~check;
+	for(i=0;i<5;i++)
+	{
+	 SoftwareSerial::write(Senddata[i]);
+    }
+}
