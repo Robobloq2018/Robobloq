6197fef7c4dbc8c11ba811215134446d0641ba9c
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-11 14:44:00 +0800

    Update RB_DCMOTOR.cpp

--- src/RB_DCMOTOR.cpp
+++ src/RB_DCMOTOR.cpp
@@ -3,10 +3,15 @@
 
 RB_DCMotor::RB_DCMotor(void):RB_SoftI2CMaster(0)
 {	
+   pinMode(20,OUTPUT);
+   digitalWrite(MOTOR_Charge_Pin,HIGH);  
 }
 
 RB_DCMotor::RB_DCMotor(uint8_t port):RB_SoftI2CMaster(port)
 {	
+   
+   pinMode(20,OUTPUT);
+   digitalWrite(MOTOR_Charge_Pin,HIGH);  
 }
 
 void RB_DCMotor::RB_DCMotor_SetSpeed(uint8_t SlaveAddress,uint8_t RegisterAddress,uint8_t Index,uint8_t Dir,uint8_t Speed)
