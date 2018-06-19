185a2438d8ec46a8755ee44a57254c3faaa14694
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-19 17:51:00 +0800

    Update RB_Servo.ino

--- example/RB_Servo/RB_Servo.ino
+++ example/RB_Servo/RB_Servo.ino
@@ -1,21 +1,23 @@
 #include "RB_SERVO.h"
 
 
-RB_SERVO  RB_Servo(1);
 
+RB_SERVO  Srvo(8);
 void setup()
 {
-  RB_Servo.RB_SERVO_Write(150,150);   //控制两个舵机
-  delay(500);
-   RB_Servo.RB_SERVO_Write_Servo1(1);       //控制1号舵机
-  delay(500);
-   RB_Servo.RB_SERVO_Write_Servo2(1);       //控制2号舵机
-  delay(500);
+  Srvo.RB_SERVO_INIT();
+  Srvo.RB_SERVO_Write(150,150);
+  delay(300);
   
 }
 
 void loop()
 {
-
+   Srvo.RB_SERVO_Write(90,90);
+   delay(1000);
+   Srvo.RB_SERVO_Write(0,0);
+   delay(1000);
+   Srvo.RB_SERVO_Write(180,180);
+  delay(1000);
 
 }
