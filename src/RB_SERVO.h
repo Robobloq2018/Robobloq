df774755e29a535fcf9a216fe5e03130f9a68795
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-19 17:52:31 +0800

    Update RB_SERVO.h

--- src/RB_SERVO.h
+++ src/RB_SERVO.h
@@ -17,8 +17,9 @@ class RB_SERVO:public RB_Port
 	
 public:	
     RB_SERVO(void);
-	  RB_SERVO(uint8_t Servo1_pin,uint8_t Servo2_pin);
+    RB_SERVO(uint8_t Servo1_pin,uint8_t Servo2_pin);
     RB_SERVO(uint8_t port);
+    void RB_SERVO::RB_SERVO_INIT(void);
     uint8_t  RB_SERVO_Attach1(int pin, int min, int max);
    uint8_t  RB_SERVO_Attach2(int pin, int min, int max);
     void RB_SERVO_Detach1(void);
