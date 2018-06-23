7caa4fb6b84e4d52303d139a251ec4e096886aa1
Robobloq2018 <37389731+Robobloq2018@users.noreply.github.com>
2018-06-23 10:47:27 +0800

    Update RB_LIGHTSENSOR.cpp

--- src/RB_LIGHTSENSOR.cpp
+++ src/RB_LIGHTSENSOR.cpp
@@ -27,3 +27,23 @@ uint16_t RB_LightSensor::GetLight(void)
 	 uint16_t value = 1024-analogRead(_SigPin);
 	 return value;
 }
+uint16_t  RB_LightSensor::  GetLightValue(void)
+{
+    int i = 0;
+    uint16_t Value[10];
+    uint16_t max = 0,min =0;
+    uint32_t sum = 0;
+    uint16_t return_value = 0;
+    for(i=0;i<10;i++) 
+       {
+          Value[i] = GetLight();
+          if(Value[i]>max)
+             max = Value[i];
+          if(Value[i]<min)
+             min = Value[i];
+          sum += Value[i];
+       }
+     sum = sum-max-min;
+     return_value = sum/8;
+     return return_value;
+}
